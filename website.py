04614cd3103e17690ad11b271b7ed88e66ec84e9
Warren Hansen <warhansen@gmail.com>
2021-04-22 15:21:38 +0200

    beginning of prject

---
+++ website.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, render_template_string, request
+
+app = Flask(__name__)
+
+
+@app.route('/get_media/')
+def get_media():
+    return render_template("/get_media.html")
+
+@app.route('/choice/', methods = ['POST', 'GET'])
+def choice():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        selection = request.form
+        if 'movies' in selection.values():
+            return render_template('/movies.html')
+        elif 'series' in selection.values():
+            return render_template('/series.html')
+
+@app.route('/movie/')
+def movies():
+    return render_template("/movies.html")
+
+@app.route('/get_movies/', methods = ['POST', 'GET'])
+def get_movies():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        movie_name = request.form
+        return movie_name.get('movie_name')
+
+@app.route('/series/')
+def series():
+    return render_template("/movies.html")
+
+@app.route('/get_series/', methods = ['POST', 'GET'])
+def get_series():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        series_name = request.form
+        return series_name.get('series_name')
+
+
+
+
+if __name__ == "__main__":
+    app.run()
