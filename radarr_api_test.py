2b83faee2b898ae72b97a230251b5f6c1b62a6e8
Warren Hansen <warhansen@gmail.com>
2021-04-23 09:05:21 +0200

    Added movie checking step in radadd_api_test before trying to add movie

--- radarr_api_test.py
+++ radarr_api_test.py
@@ -1,11 +1,20 @@
 import requests
 import json
 
+movie = '1918'
 url = "http://media:7878/api/v3/movie/?apiKey=10fc753d66e048009a1d7fbc3a8f8053"
-response = requests.get(url)
-full_list = (response.json())
 
-details = json.dumps(full_list)
+response = requests.get(url)
+full_list = str(response.json())
+
+
+if "\'title\': \'" + movie + "\'" in full_list:
+    print("found the movie the", movie, ",please go to look in Emby as it has already been added")
+else:
+    print("did not find the movie", movie,)
+
+
+
 
 
 
