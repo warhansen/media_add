32a9a6a09958c096b2dfd98e856c08419b9cf128
Warren Hansen <warhansen@gmail.com>
2021-04-25 11:22:18 +0200

    Adding more parameters, path etc

--- radarr_api_test.py
+++ radarr_api_test.py
@@ -4,11 +4,13 @@ movie = '1917'
 api_key = '10fc753d66e048009a1d7fbc3a8f8053'
 radarr_url = 'http://media:7878'
 api_url = radarr_url + "/api/v3/movie/?apiKey=" + api_key
+header = {"Content-Type": "application/json", "Accept": "text/plain"}
 quality_profile_id = 'English-Movies-1080p'
+path = '/media_server/movies'
 
 getting = requests.get(api_url)
 full_list = str(getting.json())
-params = {'title': movie, 'QualityProfileId': quality_profile_id}
+params = {'title': movie, 'QualityProfileId': quality_profile_id, 'rootFolderPath': path}
 
 
 def add_movie(movie_name):
@@ -17,7 +19,8 @@ def add_movie(movie_name):
     else:
         print("Did not find the movie", movie_name, ", it will now be added.")
 
-    requests.post(api_url, params)
+    print(requests.post(api_url, data=params, headers=header))
+    print(requests.post(api_url, data=params, headers=header).json())
 
 
 add_movie(movie)
