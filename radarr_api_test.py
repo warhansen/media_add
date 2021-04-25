1dd70b84fc299954985631f14f02ed96a935c380
Warren Hansen <warhansen@gmail.com>
2021-04-25 10:26:17 +0200

    Split up the api in sections we will use the config file for later

--- radarr_api_test.py
+++ radarr_api_test.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-movie = '1918'
+movie = '1917'
 api_key = '10fc753d66e048009a1d7fbc3a8f8053'
 radarr_url = 'http://media:7878'
 api_url = radarr_url + "/api/v3/movie/?apiKey=" + api_key
